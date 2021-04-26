ab3b352cb11aeb33bd2e48e68f769ae95057cd47
Ewhan Han <ewhan.han@gmail.com>
2021-04-26 15:28:37 -0400

    Setup Enzyme and first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('render non-empty component without crashing', () => {
+  const wrapper = shallow(<App />);
+  // throw new Error('FAIL TEST') //will fail test
+  expect(wrapper.exists()).toBe(true)
 });
